cdd40438e8f5f6da4c017c600317348f6be44450
Po An Chou <you@example.com>
2019-01-26 09:12:19 +0800

    list4 DIY modify

--- lists4/DIY/AList.java
+++ lists4/DIY/AList.java
@@ -4,30 +4,49 @@
 
 public class AList {
     /** Creates an empty list. */
+
+    private int[] item;
+    private int size;
+
     public AList() {
+        this.item = new int[100];
+        this.size = 0;
+
+    }
+    private void resize(int capacity){
+        int[] newArray = new int[capacity];
+        System.arraycopy(item, 0, newArray, 0, size);
+        item = newArray;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        if(size == item.length){
+            resize(size + 1);
+        }
+        item[size] = x;
+        size++;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return item[size];
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return item[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int last = item[size -1];
+        size--;
+        return last;
     }
 } 
